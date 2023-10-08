cc6c520941b27096afeadd007a59126951c1cedc
River <coco0958231520@gmail.com>
2023-10-08 20:08:37 +0800

    implement manifest.json and serviceWorker

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// buildExcludes is for next13
+//disable is for swGenerate multiple time warning(disable in dev)
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  sw: '/sw.js',
+  buildExcludes: ['app-build-manifest.json'],
+  disable: process.env.NODE_ENV === 'development',
+});
+module.exports = withPWA({});
